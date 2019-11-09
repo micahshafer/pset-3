cff6310d892f78970baa925c020b3ec11237bb23
micahshafer <micahshafer124@gmail.com>
2019-11-08 21:21:55 -0500

    final

--- src/change.js
+++ src/change.js
@@ -5,10 +5,24 @@ const MAX = 0.99;
 
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
+Q= .25
+D= .10
+N= .05
+P= .01
+
+
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("invalid");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("Invalid");
 } else {
-    // write your code for making change here
+  let Qu = Math.floor(amount/Q);
+    let amount1 = amount % Q;
+    let Di = Math.floor(amount1/D);
+    let amount2 = amount1 % D;
+    let Ni = Math.floor(amount1/D);
+    let amount3 = amount2 % N;
+    let Pe = Math.ceil(amount3/P);
+
+    console.log("\n" + Qu + " quarters, " + Di + "dimes, " + Ni + " nickles, and" + Pe + " pennies." );
 }
